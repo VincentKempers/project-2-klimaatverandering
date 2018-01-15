2fc4a2f0c7b065f1d96a073762364b1c8bf88c32
VincentKempers <vincent.m.kempers@gmail.com>
2018-01-15 17:03:06 +0100

    new style with fix for video

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-$(document).ready(function() {
-			$('#fullpage').fullpage({
-				sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
-				anchors: ['video', 'module', '3rdPage', '4thpage', 'lastPage'],
-				scrollingSpeed: 1000,
-				lazyLoading:true
-			});
-		});
+var myvideo = document.getElementById('myvid');
+myvideo.addEventListener('ended', function() {
+ window.location.href = 'video.html';
+});
