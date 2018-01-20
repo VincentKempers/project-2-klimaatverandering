f81500eda986194b65d5b53acc3c01a6cb0675b2
VincentKempers <vincent.m.kempers@gmail.com>
2018-01-20 17:02:52 +0100

    fixed the water-chart leave the branch open just in case we need it

--- water-chart.js
+++ water-chart.js
@@ -1,40 +1,82 @@
 var svg = d3.select("svg"),
-    margin = {
-      top: 20,
-      right: 20,
-      bottom: 30,
-      left: 40
-    },
+    margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = +svg.attr("width") - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom,
     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var x = d3.scaleBand()
-        .rangeRound([0, width])
-        .paddingInner(0.05)
-        .align(0.2);
+    .rangeRound([0, width])
+    .paddingInner(0.05)
+    .align(0.1);
 
-var y = d3.scaleLinear().rangeRound([height,0]);
-var z = d3.scaleLinear().range(["#98abc5", "#8a89a6", "#7b6888"]);
+var y = d3.scaleLinear()
+    .rangeRound([height, 0]);
 
-
+var z = d3.scaleOrdinal()
+    .range(["#50fdcc", "#3c54ff"]);
 
 d3.csv("data/water-data.csv", function(d, i, columns) {
-   for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
-    d.total = t;
-}, function(error,data) {
+  for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
+  d.total = t;
+  return d;
+}, function(error, data) {
   if (error) throw error;
 
   var keys = data.columns.slice(1);
 
-  data.sort(function(a,b){ return b.total - a. total; });
-  x.domain(data.map(function (d) { return d.product }));
-  y.domain([0, d3.max(data, function(d) {console.log(d.total)})]).nice();
+  data.sort(function(a, b) { return b.total - a.total; });
+  x.domain(data.map(function(d) { return d.Product; }));
+  y.domain([0, d3.max(data, function(d) { return d.total; })]).nice();
   z.domain(keys);
 
   g.append("g")
     .selectAll("g")
     .data(d3.stack().keys(keys)(data))
-    .enter()
-      .append("g")
-})
+    .enter().append("g")
+      .attr("fill", function(d) { return z(d.key); })
+    .selectAll("rect")
+    .data(function(d) { return d; })
+    .enter().append("rect")
+      .attr("x", function(d) { return x(d.data.Product); })
+      .attr("y", function(d) { return y(d[1]); })
+      .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+      .attr("width", x.bandwidth());
+
+  g.append("g")
+      .attr("class", "axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  g.append("g")
+      .attr("class", "axis")
+      .call(d3.axisLeft(y).ticks(10))
+    .append("text")
+      .attr("x", 12)
+      .attr("y", 1)
+      .attr("dy", "0.35em")
+      .attr("fill", "#000")
+      .attr("font-weight", "bold")
+      .attr("text-anchor", "start")
+      .text("Water in liters");
+
+      var legend = g.append("g")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 11)
+      .attr("text-anchor", "end")
+    .selectAll("g")
+    .data(keys.slice().reverse())
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+  legend.append("rect")
+      .attr("x", width - 19)
+      .attr("width", 19)
+      .attr("height", 19)
+      .attr("fill", z);
+
+  legend.append("text")
+      .attr("x", width - 24)
+      .attr("y", 9.5)
+      .attr("dy", "0.32em")
+      .text(function(d) { return d; });
+});
