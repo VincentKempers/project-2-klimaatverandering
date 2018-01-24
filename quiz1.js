00b420fcadba307494edb54b6445a6119e36dc9d
VincentKempers <vincent.m.kempers@gmail.com>
2018-01-24 22:30:53 +0100

    middag af

--- quiz1.js
+++ quiz1.js
@@ -1,23 +1,43 @@
 let buttonA =  document.getElementById('a2');
 let buttonB =  document.getElementById('b2');
+let buttonC =  document.getElementById('c2');
+let buttonD =  document.getElementById('d2');
 let tekstExplain = document.getElementById('uitleg');
 
 function changeBorderA() {
     buttonA.style.outline = "5px solid red";
-    tekstExplain.innerHTML = `
-    Bijna! Hoewel het produceren van kaas zeker een bijdrage heeft aan Co2 uitstoot komt er bij varkensvlees nog meer kijken. 
-Niet alleen is Co2 een gevolg maar ook de enorme hoeveelheid water die wij verbruiken.
-    `;
+    tekstExplain.innerHTML = `Bijna goed! Irrigatiewater komt zowel uit de grond als uit rivieren.`;
     buttonB.style.outline = "none";
+    buttonC.style.outline = "none";
+    buttonD.style.outline = "none";
 }
 
 function changeBorderB() {
-    buttonB.style.outline = "5px solid #50fdcc";
-    tekstExplain.innerHTML = `
-    Helemaal Goed! Hoewel het produceren van kaas zeker een bijdrage heeft aan Co2 uitstoot komt er bij varkensvlees nog meer kijken. Niet alleen is Co2 een gevolg maar ook de enorme hoeveelheid water die wij verbruiken.`;
+    buttonB.style.outline = "5px solid red";
+    tekstExplain.innerHTML = `Bijna goed! Irrigatiewater komt zowel uit de grond als uit rivieren.`;
     buttonA.style.outline = "none";
+    buttonC.style.outline = "none";
+    buttonD.style.outline = "none";
+}
+
+function changeBorderC() {
+    buttonC.style.outline = "5px solid #50fdcc";
+    tekstExplain.innerHTML = `Heel goed! Irrigatiewater komt zowel uit de grond als uit rivieren.`;
+    buttonA.style.outline = "none";
+    buttonB.style.outline = "none";
+    buttonD.style.outline = "none";
+}
+
+function changeBorderD() {
+    buttonD.style.outline = "5px solid red";
+    tekstExplain.innerHTML = `Bijna goed! Irrigatiewater komt zowel uit de grond als uit rivieren.`;
+    buttonA.style.outline = "none";
+    buttonB.style.outline = "none";
+    buttonC.style.outline = "none";
 }
 
 
 buttonA.addEventListener('click', changeBorderA, true);
 buttonB.addEventListener('click', changeBorderB, true);
+buttonC.addEventListener('click', changeBorderC, true);
+buttonD.addEventListener('click', changeBorderD, true);
